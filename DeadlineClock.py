3b05d1ebb6f94f0273ae8b744e3e329cf313c476
Ruairi Sinkler <risinkler1@sheffield.ac.uk>
2019-04-24 17:28:31 +0100

    Exam Deadlines added

--- DeadlineClock.py
+++ DeadlineClock.py
@@ -16,7 +16,7 @@ class Deadline:
 
     def toString(self):
         seconds, minutes, hours, days = self.getTimes()
-        resultString = ("\n\n{:<50}{:>50}\n\n").format(self.name, "Deadline: " + self.deadline.strftime("%X, %A %d %B %Y"))
+        resultString = ("\n{:<50}{:>50}\n\n").format(self.name, "Deadline: " + self.deadline.strftime("%X, %A %d %B %Y"))
 
         resultString += ("{:<15} {:<10}").format("Total seconds:", seconds)
         resultString += ("{:<15} {:<10}").format("Total minutes:", minutes)
@@ -29,19 +29,38 @@ class Deadline:
 
 
 dissertation = Deadline("Dissertation", datetime.datetime(2019, 5, 1, 15))
-natural_systems = Deadline("Natural Systems", datetime.datetime(2019, 3, 18, 15))
+natural_systems = Deadline("Natural Systems - ABM", datetime.datetime(2019, 3, 18, 15))
 intelligent_web_part_1 = Deadline("Intelligent Web Part 1", datetime.datetime(2019, 4, 1, 23, 59, 59))
 intelligent_web_part_2 = Deadline("Intelligent Web Part 2", datetime.datetime(2019, 5, 17, 23, 59, 59))
-adaptive_intelligence = Deadline("Adaptive Intelligence", datetime.datetime(2019, 4, 1, 15))
+adaptive_intelligence = Deadline("Adaptive Intelligence - Letters", datetime.datetime(2019, 4, 1, 15))
+adaptive_intelligence_2 = Deadline("Adaptive Intelligence - Chess", datetime.datetime(2019, 5, 13, 23, 59, 59))
 
-all_deadlines = [dissertation, natural_systems, intelligent_web_part_1, intelligent_web_part_2, adaptive_intelligence]
+all_deadlines = [dissertation, natural_systems, intelligent_web_part_1, intelligent_web_part_2, adaptive_intelligence, adaptive_intelligence_2]
 all_deadlines.sort(key=lambda x: x.deadline)
+
+natural_systems_exam = Deadline("Natural Systems Exam", datetime.datetime(2019, 6, 7, 9))
+adaptive_intelligence_exam = Deadline("Adaptive Intelligence Exam", datetime.datetime(2019, 5, 29, 9))
+security_exam = Deadline("Computer Security & Forensics Exam", datetime.datetime(2019, 6, 1, 9))
+
+all_exams = [natural_systems_exam, adaptive_intelligence_exam, security_exam]
+all_exams.sort(key=lambda x: x.deadline)
 os.system('clear')
 
 while(True):
     try:
-        for deadline in all_deadlines:
-            print(deadline.toString())
+        print("="*100)
+        all_deadlines = [deadline for deadline in all_deadlines if deadline.getTimes()[0] > 0]
+        if (all_deadlines):
+            print("DEADLINES")
+            print("="*100)
+            for deadline in all_deadlines:
+                print(deadline.toString())
+        all_exams = [exam for exam in all_exams if exam.getTimes()[0] > 0]
+        if (all_exams):
+            print("EXAMS")
+            print("="*100)
+            for exam in all_exams:
+                print(exam.toString())
         time.sleep(1)
         os.system('clear')
     except KeyboardInterrupt:
